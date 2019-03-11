7092f0d60ab4d1066e15a332d19abe8a915dfeae
SwapnaliTTechnocredits <swapnalitalekar1@gmail.com>
2019-03-11 23:06:54 +0530

    Assignment 1 Swapnali

---
+++ seleniumByTechnocredits/src/seleniumPractise/swapanali/Q4_JavaScript_Prompt.java
@@ -0,0 +1,38 @@
+package seleniumPractise.swapanali;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import util.Init;
+
+public class Q4_JavaScript_Prompt {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver= Init.initChromeDriver();
+		javaScriptPromt_Msg(driver);
+		driver.close();
+	}
+	public static void javaScriptPromt_Msg(WebDriver driver) throws InterruptedException{
+		
+		driver.manage().window().maximize();
+		driver.get("http://automationbykrishna.com/#");
+		driver.findElement(By.partialLinkText("asic Ele")).click();
+		Thread.sleep(3000);
+		System.out.println("Basic element window");
+		Thread.sleep(3000);
+		
+		WebElement element=driver.findElement(By.xpath("//button[starts-with(@id,'javascriptC')]"));
+		JavascriptExecutor js=(JavascriptExecutor)driver;
+		js.executeScript("arguments[0].scrollIntoView(true)", element);
+		driver.findElement(By.xpath("//button[@id='javascriptPromp']")).click();
+		Alert alert=driver.switchTo().alert();
+		String input="Swapnali";
+		alert.sendKeys(input);
+		alert.accept();
+		String msg="Hello ".concat(input).concat(" How are you today?");
+		System.out.println(msg);
+	}
+}
---
+++ seleniumByTechnocredits/src/seleniumPractise/swapanali/Q5_LoginWindowMsg.java
@@ -0,0 +1,43 @@
+package seleniumPractise.swapanali;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import util.Init;
+
+public class Q5_LoginWindowMsg {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = Init.initChromeDriver();
+		loginWindow_msg(driver);
+		driver.close();
+	}
+
+	public static void loginWindow_msg(WebDriver driver) throws InterruptedException {
+
+		driver.manage().window().maximize();
+		driver.get("http://automationbykrishna.com/#");
+		driver.findElement(By.xpath("//a[contains(@id,'registration2')]")).click();
+		Thread.sleep(3000);
+		System.out.println("Registration window");
+		Thread.sleep(3000);
+
+		String pwd = "Sw@pnali1";
+		driver.findElement(By.xpath("//input[starts-with(@id,'unameSignin')]")).sendKeys("Swapnali");
+		driver.findElement(By.xpath("//input[starts-with(@id,'pwdSignin')]")).sendKeys(pwd);
+		if (pwd.trim().isEmpty()) {
+			System.out.println("Please enter password");
+		} else if (!pwd.isEmpty() && pwd.length() <= 8) {
+			System.out.println("Please enter strong password to continue login!!!");
+		} else {
+			driver.findElement(By.xpath("//input[starts-with(@id,'pwdSignin')]")).sendKeys(pwd);
+			driver.findElement(By.id("btnsubmitdetails")).click();
+			Alert alert1 = driver.switchTo().alert();
+			System.out.println(alert1.getText());
+			alert1.accept();
+
+		}
+	}
+
+}
